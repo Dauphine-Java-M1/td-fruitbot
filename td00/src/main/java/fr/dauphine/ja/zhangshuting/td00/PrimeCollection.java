4c95f75246fe3c6916a0d14604c5bae2d2cb887c
fruitbot <shutingzhang110@gmail.com>
2020-09-17 16:28:00 +0200

    initial im- port

---
+++ td00/src/main/java/fr/dauphine/ja/zhangshuting/td00/PrimeCollection.java
@@ -0,0 +1,58 @@
+package fr.dauphine.ja.zhangshuting.td00;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Random;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection 
+{
+	java.util.ArrayList<Integer> numbers;
+	
+	public PrimeCollection() {
+		numbers = new ArrayList<Integer>();
+	}
+	
+	public void initRandom(int n, int m) {
+        Random ran = new Random(); 
+        for (int i = 0; i < n; i++) {
+            int nxt = ran.nextInt(m); 
+            this.numbers.add(nxt);
+        }
+	}
+	private static boolean isPrime(int p) {
+		boolean flag = false;
+		if(p==0 || p==1){  
+			return false;
+		} 
+		for(int i=2;i<=p/2;i++){      
+			if(p%i == 0){      
+				flag = true;  
+				break;			    
+			}      
+		}      
+		if(!flag){ 
+			return true; 
+		}else {
+			return false;
+		}
+	}
+	public void printPrimes() {
+		numbers.forEach(number ->{
+			if(isPrime(number) == true) {
+	            System.out.println(number);
+			}
+        });
+	}
+	
+    public static void main( String[] args )
+    {
+    	PrimeCollection p = new PrimeCollection();
+    	p.initRandom(100, 1000);
+    	p.printPrimes();	
+    }
+}
